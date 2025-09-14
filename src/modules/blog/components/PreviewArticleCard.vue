927f41b10db03cc86e5288734e901a738d712d42
Mariam Rosal <mariamrosalm@gmail.com>
2025-09-14 16:51:08 -0400

    feat: agregar componente PreviewArticleCard y actualizar la página de inicio del blog

---
+++ src/modules/blog/components/PreviewArticleCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+interface Article {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  date: string;
+  image: string;
+  likes: number;
+  comments: number;
+}
+
+interface Props {
+  article: Article;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <article class="bg-dark q-pa-lg rounded-borders q-mb-sm">
+    <div class="flex items-center text-caption q-gutter-sm">
+      <q-avatar size="48px">
+        <img :src="props.article.image" />
+      </q-avatar>
+      <span class="text-weight-bold">{{ props.article.author }}</span>
+      <time :datetime="props.article.date">Hace 3 horas</time>
+    </div>
+
+    <h2>{{ props.article.title }}</h2>
+    <p>
+      {{ props.article.content }}
+    </p>
+    <q-img height="400px" :src="props.article.image" class="rounded-borders" />
+    <div class="q-gutter-sm q-mt-md">
+      <q-btn flat icon="sym_r_thumb_up" :label="props.article.likes" color="primary" />
+      <q-btn flat icon="sym_r_comment" :label="props.article.comments" color="primary" />
+      <q-btn flat icon="sym_r_share" label="Compartir" color="primary" no-caps />
+    </div>
+  </article>
+</template>
+
+<style scoped></style>
--- src/modules/blog/pages/HomeBlogPage.vue
+++ src/modules/blog/pages/HomeBlogPage.vue
@@ -1,33 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PreviewArticleCard from '../components/PreviewArticleCard.vue';
+
+const articles = [
+  {
+    id: 1,
+    title: 'Entendiendo los Hooks de React',
+    content:
+      'Los Hooks son una nueva incorporación en React 16.8. Te permiten usar estado y otras características de React sin escribir una clase. En este post, exploraremos `useState`, `useEffect` y `useContext` ...',
+    author: 'Jonh Doe',
+    date: '2025-09-01T12:00:00Z',
+    image: 'https://cdn.quasar.dev/img/avatar.png',
+    likes: 4,
+    comments: 2,
+  },
+  {
+    id: 2,
+    title: 'Guía de inicio a Tailwind CSS',
+    content:
+      'Tailwind CSS es un framework de CSS "utility-first" para un rápido desarrollo de interfaces de usuario. A diferencia de otros frameworks como Bootstrap, no viene con componentes pre-diseñados. En su lugar, proporciona clases de utilidad que puedes combinar para crear diseños personalizados.',
+    author: 'Jane Smith',
+    date: '2025-09-02T12:00:00Z',
+    image: 'https://cdn.quasar.dev/img/avatar.png',
+    likes: 5,
+    comments: 3,
+  },
+];
+</script>
 
 <template>
   <q-page class="q-pa-lg q-pb-lg row q-col-gutter-md">
-    <section class="col-sm-9 col-lg-8 col-xl-9 flex justify-center">
-      <article class="bg-dark q-pa-lg rounded-borders">
-        <div class="flex items-center text-tiny q-gutter-sm">
-          <q-avatar size="48px">
-            <img src="https://cdn.quasar.dev/img/avatar.png" />
-          </q-avatar>
-          <span class="text-weight-bold">Jonh Doe</span>
-          <time datetime="2025-09-01T12:00:00Z">Hace 2 horas</time>
-        </div>
-
-        <h2>Entendiendo los Hooks de React</h2>
-        <p>
-          Los Hooks son una nueva incorporación en React 16.8. Te permiten usar estado y otras
-          características de React sin escribir una clase. En este post, exploraremos `useState`,
-          `useEffect` y `useContext` ...
-        </p>
-        <q-img height="400px" src="https://cdn.quasar.dev/img/avatar.png" class="rounded-borders" />
-        <!-- <q-separator class="" /> -->
-        <div class="q-gutter-sm q-mt-md">
-          <q-btn flat icon="sym_r_thumb_up" label="4" color="primary" />
-          <q-btn flat icon="sym_r_comment" label="2" color="primary" />
-          <q-btn flat icon="sym_r_share" label="Compartir" color="primary" no-caps />
-        </div>
-      </article>
+    <section class="col-sm-12 col-md-8 col-lg-8 col-xl-9 flex justify-center">
+      <PreviewArticleCard v-for="article in articles" :key="article.id" :article="article" />
     </section>
-    <section class="col-sm-3 col-lg-4 col-xl-3">
+    <section class="col-sm-3 col-md-4 col-lg-4 col-xl-3 gt-sm">
       <!-- sidebar -->
       <q-card flat class="bg-dark rounded-borders">
         <q-card-section>
